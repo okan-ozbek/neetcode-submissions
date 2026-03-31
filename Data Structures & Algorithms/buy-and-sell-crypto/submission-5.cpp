b502e565204746d8621bdf9f204d94d3d817f1f8
Okan Ozbek <127297261+okan-ozbek@users.noreply.github.com>
2026-03-31 15:12:01 +0200

    Add: buy-and-sell-crypto - submission-5

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-5.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buyIndex{0};
+        int sellIdex{0};
+
+        int maxProfit{0};
+
+        while (sellIdex < prices.size()) {
+            if (prices[buyIndex] > prices[sellIdex]) {
+                buyIndex++;
+            } else if (prices[buyIndex] < prices[sellIdex]) {
+                int currentSell{prices[sellIdex] - prices[buyIndex]};
+
+                if (maxProfit < currentSell) {
+                    maxProfit = currentSell;
+                }
+
+                sellIdex++;
+            } else {
+                sellIdex++;
+            }
+        }
+
+        return maxProfit;
+    }
+};
