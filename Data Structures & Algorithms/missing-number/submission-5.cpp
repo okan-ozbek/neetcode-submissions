96dd627f612248063d6146efc28ff2495e64c2b2
Okan Ozbek <127297261+okan-ozbek@users.noreply.github.com>
2026-04-04 18:29:57 +0200

    Add: missing-number - submission-5

---
+++ Data Structures & Algorithms/missing-number/submission-5.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        std::vector x(nums.size(), 0);
+
+        for (auto& n : nums) {
+            ++x[n];
+        }
+
+        for (int i{}; i < x.size(); ++i) {
+            if (x[i] == 0) {
+                return i;
+            }
+        }
+
+        return x.size();
+    }
+};
