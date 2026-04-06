65596422816124d24349506713f42882120e1d6c
Okan Ozbek <127297261+okan-ozbek@users.noreply.github.com>
2026-04-06 02:11:26 +0200

    Add: singlyLinkedList - submission-7

---
+++ Data Structures & Algorithms/singlyLinkedList/submission-7.cpp
@@ -0,0 +1,106 @@
+// Singly Linked List Node
+class ListNode {
+public:
+    int val;
+    ListNode* next;
+
+    ListNode(int val) : val(val), next(nullptr) {}
+    ListNode(int val, ListNode* next) : val(val), next(next) {}
+};
+  
+class LinkedList {
+private:
+    ListNode* head;
+    ListNode* tail;
+
+public:
+    LinkedList() : head(nullptr), tail(nullptr) {}
+
+    int get(int index) {
+        int iteration{0};
+        auto* node{head};
+
+        while (node != nullptr) {
+            if (iteration == index) {
+                return node->val;
+            }
+
+            ++iteration;
+            node = node->next;
+        }
+        
+        return -1;
+    }
+
+    void insertHead(int val) {
+        auto* node = new ListNode(val);
+
+        node->next = head;
+        head = node;
+
+        if (node->next == nullptr) {
+            tail = node;
+        }
+    }
+    
+    void insertTail(int val) {
+        auto* node = new ListNode(val);
+
+        if (tail == nullptr) {
+            head = node;
+            tail = node;
+            return;
+        }
+
+        tail->next = node;
+        tail = tail->next;
+    }
+
+    bool remove(int index) {
+        int iteration{0};
+        auto* node{head};
+
+        if (index == 0 && head != nullptr) {
+            auto* deletable = head;
+            head = head->next;
+
+            if (head == nullptr) {
+                tail = nullptr;
+            }
+
+            delete deletable;
+            return true;
+        }
+
+        while (iteration + 1 < index && node != nullptr) {
+            ++iteration;
+            node = node->next;
+        }
+
+        if (node != nullptr && node->next != nullptr) {
+            if (node->next == tail) {
+                tail = node;
+            }
+
+            auto* deletable = node->next;
+            node->next = node->next->next;
+
+            delete deletable;
+            return true;
+        }
+
+        return false;
+    }
+
+    vector<int> getValues() {
+        vector<int> result{};
+        auto* node{head};
+
+        while (node != nullptr) {
+            result.push_back(node->val);
+            node = node->next;
+        }
+
+        return result;
+    }
+};
