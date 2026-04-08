c9559ad0da06753eb9b0ce1755d6ee4ea9911fcc
Okan Ozbek <127297261+okan-ozbek@users.noreply.github.com>
2026-04-09 00:08:34 +0200

    Add: two-integer-sum-ii - submission-9

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-9.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l{0}, r = numbers.size() - 1;
+        vector<int> res(2, 0);
+
+        while (l < r) {
+            int sum{numbers[l] + numbers[r]};
+
+            if (sum == target) {
+                res[0] = l+1;
+                res[1] = r+1;
+                break;
+            }
+
+            if (sum > target) {
+                --r;
+            } 
+            else {
+                ++l;
+            }
+        }
+
+        return res;
+    }
+};
