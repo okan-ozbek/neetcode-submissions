aad279ef6528119b5658f6cbc740ea8ed145ad16
Okan Ozbek <127297261+okan-ozbek@users.noreply.github.com>
2026-04-09 16:21:32 +0200

    Add: find-duplicate-integer - submission-8

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-8.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        std::vector seen(nums.size(), 0);
+
+        for(int x : nums) {
+            seen[x]++;
+
+            if (seen[x] >= 2) return x;
+        }
+
+        return -1;
+    }
+};
