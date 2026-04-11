c0086778458e9a7df69bcb4d17f2592fe141004f
Okan Ozbek <127297261+okan-ozbek@users.noreply.github.com>
2026-04-12 01:44:59 +0200

    Add: duplicate-integer - submission-7

---
+++ Data Structures & Algorithms/duplicate-integer/submission-7.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        std::unordered_map<int, int> um;
+
+        for (int x : nums) {
+            ++um[x];
+            if (um[x] >= 2) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
