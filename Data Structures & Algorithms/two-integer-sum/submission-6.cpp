c3d1161cf732c98b01116046c30309a4ed10d4c0
Okan Ozbek <127297261+okan-ozbek@users.noreply.github.com>
2026-04-12 02:02:48 +0200

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> um{};
+
+        for(int i{}; i < nums.size(); ++i) {
+            if (um.find(target - nums[i]) != um.end()) {
+                vector<int> v{};
+                v.push_back(um[target - nums[i]]);
+                v.push_back(i);
+
+                return v;
+            }
+
+            if (um.find(nums[i]) == um.end()) {
+                um[nums[i]] = i;
+            }
+        }
+    }
+};
