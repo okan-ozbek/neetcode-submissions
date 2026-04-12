8d3f04b1a2704be309923c95ff93e91fc1d36513
Okan Ozbek <127297261+okan-ozbek@users.noreply.github.com>
2026-04-12 02:14:20 +0200

    Add: string-encode-and-decode - submission-35

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-35.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string encoded{};
+
+        for(const string& str : strs) {
+            encoded += to_string(str.length()) + "#" + str;
+        }
+
+        return encoded;
+    }
+
+    vector<string> decode(string s) {
+        std::vector<string> res{};
+        int offset{0};
+
+        while (offset < s.length()) {
+            int numLength{offset};
+
+            while(s[numLength] != '#') {
+                ++numLength;
+            }
+
+            int wordLength{std::stoi(s.substr(offset, numLength - offset))};
+
+            res.push_back(s.substr(numLength + 1, wordLength));
+            offset = numLength + 1 + wordLength;
+        }
+
+        return res;
+    }
+};
