53cfbfc434a783b204f2968a6b4d11de8ffd47be
Okan Ozbek <127297261+okan-ozbek@users.noreply.github.com>
2026-04-12 02:15:05 +0200

    Add: products-of-array-discluding-self - submission-5

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-5.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> results{};
+
+        for(int i{}; i < n; ++i) {
+            int sum{1};
+
+            for (int j{}; j < n; ++j) {
+                if (j == i) {
+                    continue;
+                }
+
+                sum *= nums[j];
+            }
+
+            results.push_back(sum);
+        }
+
+        return results;
+    }
+};
