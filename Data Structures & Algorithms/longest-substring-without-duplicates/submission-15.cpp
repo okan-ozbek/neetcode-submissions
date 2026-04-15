55d21393a78e5c31889a1011c29c3e450d44aaff
Okan Ozbek <127297261+okan-ozbek@users.noreply.github.com>
2026-04-15 09:14:29 +0200

    Add: longest-substring-without-duplicates - submission-15

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-15.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    // int lengthOfLongestSubstring(string s) {
+    //     std::unordered_map<char, int> um{};
+    //     int l{0}, res{0};
+
+    //     for(int r{}; r < s.size(); ++r) {
+    //         if (um.find(s[r]) != um.end()) {
+    //             l = std::max(um[s[r]] + 1, l);
+    //         } 
+            
+    //         um[s[r]] = r;
+    //         res = std::max(res, r - l + 1);
+    //     }
+
+    //     return res;
+    // }   
+
+    int lengthOfLongestSubstring(string s) {
+        std::unordered_map<char, int> u_map{};
+        int left{0}, result{0};
+
+        for (int right{}; right < s.size(); right++) {
+            if (u_map.find(s[right]) != u_map.end()) {
+                left = std::max(left, u_map[s[right]] + 1);
+            }
+
+            u_map[s[right]] = right;
+
+            if (right - left + 1 >= result) {
+                result = right - left + 1;
+            } 
+        }
+
+        return result;
+    }
+};
