ca188b3ca6afddf3615f92b1861134000bf699ea
Okan Ozbek <127297261+okan-ozbek@users.noreply.github.com>
2026-05-05 17:36:46 +0200

    Add: design-hashset - submission-1

---
+++ Data Structures & Algorithms/design-hashset/submission-1.cpp
@@ -0,0 +1,37 @@
+class MyHashSet {
+public:
+    MyHashSet() : m_data(DEFAULT_CAPACITY, -1) {}
+    
+    void add(const int key) {
+        if (m_data[hash(key)] == -1) {
+            m_data[hash(key)] == 0;
+        }
+
+        ++m_data[hash(key)];
+    }
+    
+    void remove(const int key) {
+        m_data[hash(key)] = -1;
+    }
+    
+    bool contains(const int key) const {
+        return (m_data[hash(key)] != -1);
+    }
+
+private: 
+    constexpr static int DEFAULT_CAPACITY{ 1007 };
+
+    std::vector<int> m_data;
+
+    const int hash(const int key) const {
+        return key % DEFAULT_CAPACITY;
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
