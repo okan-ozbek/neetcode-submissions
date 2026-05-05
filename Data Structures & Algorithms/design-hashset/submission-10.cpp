dc3f636b8ee5511039c2799b0efa62936e5faa38
Okan Ozbek <127297261+okan-ozbek@users.noreply.github.com>
2026-05-05 17:47:35 +0200

    Add: design-hashset - submission-10

---
+++ Data Structures & Algorithms/design-hashset/submission-10.cpp
@@ -0,0 +1,49 @@
+class MyHashSet {
+public:
+    MyHashSet() : m_data{ static_cast<int*>(::operator new(DEFAULT_CAPACITY * sizeof(int))) } {
+        std::fill_n(m_data, DEFAULT_CAPACITY, -1);
+    }
+
+    MyHashSet(const MyHashSet&) = delete;
+    MyHashSet(MyHashSet&&) noexcept = delete;
+
+    ~MyHashSet() {
+        delete[] m_data;
+    }
+
+    MyHashSet& operator=(const MyHashSet&) = delete;
+    MyHashSet& operator=(MyHashSet&&) noexcept = delete;
+    
+    void add(const int key) {
+        if (m_data[hash(key)] == -1) {
+            m_data[hash(key)] == 0;
+        }
+
+        ++m_data[hash(key)];
+    }
+    
+    void remove(const int key) {
+        m_data[hash(key)] = -1;
+    }
+    
+    bool contains(const int key) const {
+        return (m_data[hash(key)] != -1);
+    }
+
+private: 
+    constexpr static int DEFAULT_CAPACITY{ 1007 };
+
+    int* m_data;
+
+    const int hash(const int key) const {
+        return key % DEFAULT_CAPACITY;
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
