d3ec3a88ec9924fa7d8515ed9e6f834d50062cb2
Okan Ozbek <127297261+okan-ozbek@users.noreply.github.com>
2026-05-06 22:23:47 +0200

    Add: sort-an-array - submission-1

---
+++ Data Structures & Algorithms/sort-an-array/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        for (std::size_t i{ 1 }; i < nums.size(); ++i) {
+            std::size_t j{ i - 1 };
+
+            while ((j >= 0) && (nums[j + 1] < nums[j])) {
+                swap(nums[j + 1], nums[j]);
+                if (j == 0) break;
+
+                --j;
+            }
+        }
+
+        return nums;
+    }
+
+    void swap(int& a, int& b) {
+        auto c = a;
+
+        a = b;
+        b = c;
+    }
+};
