1cfe0fba3c8a389fc1656ea8cb718445ee696432
Okan Ozbek <127297261+okan-ozbek@users.noreply.github.com>
2026-05-06 22:03:06 +0200

    Add: sort-colors - submission-3

---
+++ Data Structures & Algorithms/sort-colors/submission-3.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) noexcept {
+        bool sorted = false;
+
+        do {
+            sorted = true;
+
+            for (std::size_t i{1}; i < nums.size(); ++i) {
+                if (nums[i - 1] > nums[i]) {
+                    swap(nums[i - 1], nums[i]);
+                    sorted = false;
+                }
+            }
+        } 
+        while(sorted == false);
+    }
+
+    void swap(int& a, int& b) noexcept {
+        auto c = a;
+        a = b;
+        b = c;
+    }
+};
