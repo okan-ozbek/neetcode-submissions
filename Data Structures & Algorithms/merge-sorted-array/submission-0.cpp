9568067d03ce023af1dbccea7876447fe296f727
Okan Ozbek <127297261+okan-ozbek@users.noreply.github.com>
2026-05-06 22:29:49 +0200

    Add: merge-sorted-array - submission-0

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        for (std::size_t i{}; i < n; ++i) {
+            nums1[m + i] = nums2[i];
+        }
+
+        for (std::size_t i{ 1 }; i < m + n; ++i) {
+            std::size_t j{ i - 1 };
+
+            while (j >= 0 && nums1[j + 1] < nums1[j]) {
+                std::swap(nums1[j + 1], nums1[j]);
+                
+                if (j == 0) {
+                    break;
+                }
+            
+                --j;
+            }
+        }
+    }
+};
