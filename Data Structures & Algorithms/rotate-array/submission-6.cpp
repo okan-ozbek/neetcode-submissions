b884bac9bc90f515aa953fa3b3f4573a3c916280
Okan Ozbek <127297261+okan-ozbek@users.noreply.github.com>
2026-05-08 21:38:39 +0200

    Add: rotate-array - submission-6

---
+++ Data Structures & Algorithms/rotate-array/submission-6.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        for (int i = 0; i < k; ++i) {
+            int curr = nums[nums.size() - 1];
+            for (int j = 0; j < nums.size(); ++j) {
+                int temp = nums[j];
+                nums[j] = curr;
+                curr = temp;
+            }
+        }
+    }
+};
