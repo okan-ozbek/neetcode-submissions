087c5570a2c638b53045bfe1058fd08056e3f6a8
Okan Ozbek <127297261+okan-ozbek@users.noreply.github.com>
2026-05-12 02:16:43 +0200

    Add: online-stock-span - submission-6

---
+++ Data Structures & Algorithms/online-stock-span/submission-6.cpp
@@ -0,0 +1,30 @@
+class StockSpanner {
+public:
+    StockSpanner() = default;
+    
+    int next(int price) {
+        if (monotonic_stack.empty()) {
+            monotonic_stack.push({ price, 1 });
+            return 1;
+        }
+
+        int span{ 1 };
+        while (!monotonic_stack.empty() && monotonic_stack.top().first <= price) {
+            auto values = monotonic_stack.top();
+            span += values.second;
+            monotonic_stack.pop();
+        }
+
+        monotonic_stack.push({ price, span });
+        return span;
+    }
+
+private:
+    std::stack<std::pair<int, int>> monotonic_stack{};
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
