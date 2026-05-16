e61bc8b9b2e74ac11e917a5bf38038e501c3c3ef
Okan Ozbek <127297261+okan-ozbek@users.noreply.github.com>
2026-05-16 13:32:46 +0200

    Add: guess-number-higher-or-lower - submission-11

---
+++ Data Structures & Algorithms/guess-number-higher-or-lower/submission-11.cpp
@@ -0,0 +1,23 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        if (guess(n) == 0) return n;
+
+        while(true) {
+            int r{ guess(n) };
+            
+            if (r == 0) return n;
+            else if (r == -1) n = n / 2;
+            else n = (n + 1) * 2;
+        }
+    }
+};
