9eb7ba4d34db50f691c591f01b6caafe1826f9fc
Okan Ozbek <127297261+okan-ozbek@users.noreply.github.com>
2026-05-18 16:48:11 +0200

    Add: majority-element-ii - submission-2

---
+++ Data Structures & Algorithms/majority-element-ii/submission-2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        std::size_t n{ nums.size() };
+        std::size_t calc{ n / 3 }; 
+        std::unordered_map<int, int> um{};
+
+        for(std::size_t i{}; i < n; ++i) {
+            ++um[nums[i]];
+        }
+
+        std::vector<int> res{};
+        for(auto& [key, count] : um) {
+            if (count > calc) {
+                res.push_back(key);
+            }
+        }
+
+        return res;
+    }
+};
