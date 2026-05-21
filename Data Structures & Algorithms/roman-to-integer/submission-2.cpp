8fd89deeda0f1f3831d17f5d430dda7450a75b36
Okan Ozbek <127297261+okan-ozbek@users.noreply.github.com>
2026-05-22 01:27:08 +0200

    Add: roman-to-integer - submission-2

---
+++ Data Structures & Algorithms/roman-to-integer/submission-2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> um = {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 } 
+        };
+
+        int res{};
+        for (int i{}; i < s.size(); ++i) {
+            if (i + 1 != s.size() && um[s[i]] < um[s[i + 1]]) {
+                res = res + (um[s[i + 1]] - um[s[i]]);
+                ++i;
+            }
+            else {
+                res += um[s[i]];
+            }
+        }
+
+        return res;
+    }
+};
