5d52999d486abbf15581da77146df7dca5720795
Okan Ozbek <127297261+okan-ozbek@users.noreply.github.com>
2026-05-22 15:01:30 +0200

    Add: ransom-note - submission-2

---
+++ Data Structures & Algorithms/ransom-note/submission-2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int> mag{};
+
+        for (auto& chr : magazine) {
+            ++mag[chr];
+        }
+
+        for (auto& chr : ransomNote) {
+            auto it = mag.find(chr);
+            if (it == mag.end() || it->second <= 0) {
+                return false;
+            }
+            it->second--;
+        }
+
+        return true;
+    }
+};
