6c7835fc568140d7736190d6b7fec90735168710
Okan Ozbek <127297261+okan-ozbek@users.noreply.github.com>
2026-06-01 23:40:55 +0200

    Add: crawler-log-folder - submission-2

---
+++ Data Structures & Algorithms/crawler-log-folder/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int res{};
+        for (string& x : logs) {
+            if (x == "../") {
+                res = std::max(0, --res);
+            }
+            else if (x != "./") {
+                ++res;
+            }
+
+            
+        }
+
+        return std::max(0, res);
+    }
+};
